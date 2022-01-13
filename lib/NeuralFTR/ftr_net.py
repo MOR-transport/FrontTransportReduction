d9af328e87d0df8d9de92008e3ef4d13ba8dcf1c
Steffen <steffen.buechholz@outlook.de>
2022-01-13 12:02:05 +0100

    lots of unused stuff removed

--- lib/NeuralFTR/ftr_net.py
+++ lib/NeuralFTR/ftr_net.py
@@ -19,147 +19,30 @@ class BaseModule(nn.Module):
         self.load_state_dict(torch.load(fpath, map_location=DEVICE))
     
     
-class ConvTConv(BaseModule):
-    def __init__(self, kernel_size=[5, 11], channels=[1, 16, 1]):
-        super(self.__class__, self).__init__()
-        self.conv = nn.Conv2d(in_channels=channels[0], out_channels=channels[1], kernel_size=kernel_size[0],
-                              padding=int((kernel_size[0] - 1)/2), padding_mode='replicate')
-        self.bn = nn.BatchNorm2d(channels[1])
-        self.pad = nn.ReplicationPad2d(int((kernel_size[1]-1)/2))
-        self.unpad_size = kernel_size[1] - 1
-        self.Tconv = nn.ConvTranspose2d(in_channels=channels[1], out_channels=channels[-1], kernel_size=kernel_size[1])
-
-    def forward(self, x):
-        out = self.act(self.bn(self.conv(x)))
-        out = self.Tconv(self.pad(out))[..., self.unpad_size:-self.unpad_size, self.unpad_size:-self.unpad_size]
-    
-        return out
-
-# class ODE_Block(BaseModule):
-#     def __init__(self,)
-
-class Phi2(BaseModule):
-    def __init__(self, n_layers=16, f=nn.Sigmoid):
-        super(self.__class__, self).__init__()
-        self.has_bottleneck = False
-        self.f = f()
-        self.instnorm = nn.InstanceNorm2d(1)
-        #layer_act_bn = nn.Sequential(ConvTConv(), nn.BatchNorm2d(1), self.act)
-        self.layers = nn.ModuleList([nn.Sequential(ConvTConv(), nn.BatchNorm2d(1), self.act) for _ in range(n_layers-1)])
-        self.layers.append(ConvTConv())
-        
-    def forward(self, q, apply_f=True, return_phi=True):
-        phi = self.instnorm(q)
-        for layer in self.layers:
-            phi = layer(phi)
-        if apply_f and return_phi:
-            return phi, self.f(phi)
-        if apply_f:
-            return self.f(phi)
-        return phi
-    
-    
-class ResLayer(BaseModule):
-    def __init__(self, kernel_size=3, channels=16, T=False):
-        super(self.__class__, self).__init__()
-        channels = [channels] if isinstance(channels, int) else channels
-        #self.pad1 = nn.ReplicationPad2d(int((kernel_size-1)/2))
-        self.conv1 = nn.Conv2d(in_channels=channels[0], out_channels=channels[-1], kernel_size=kernel_size,
-                                            padding=int((kernel_size - 1)/2), padding_mode='replicate')
-        self.bn1 = nn.BatchNorm2d(channels[-1])
-        if T:
-            self.conv2 = nn.ConvTranspose2d(in_channels=channels[-1], out_channels=channels[0], kernel_size=kernel_size,
-                                            padding=int((kernel_size - 1)/2))
-        else:
-            self.conv2 = nn.Conv2d(in_channels=channels[-1], out_channels=channels[0], kernel_size=kernel_size,
-                                    padding=int((kernel_size - 1)/2), padding_mode='replicate')
-        self.bn2 = nn.BatchNorm2d(channels[0])
-        
-    def forward(self, x):
-        
-        out = self.bn1(self.act(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        
-        return x + out
-    
-    
-class ResBlock(BaseModule):
-    def __init__(self, kernel_size=3, channels=16, layers=5, act_output=True):
-        super(self.__class__, self).__init__()
-        channels = [channels] * 2 if isinstance(channels, int) else channels
-        seq_layers = [nn.Sequential(ResLayer(kernel_size, channels), self.act, nn.BatchNorm2d(channels[0]))
-                      for _ in range(layers - act_output)]
-        if not act_output:
-            seq_layers.append(ResLayer(kernel_size, channels))
-            
-        self.block = nn.Sequential(*seq_layers)
-    
-    def forward(self, x):
-        return self.block(x)
-        
-        
-class Phi(BaseModule):
-    def __init__(self, kernel_size=9, channels=[8], blocks=4, layers_per_block=3, f=nn.Sigmoid):
-        super(self.__class__, self).__init__()
-        self.has_bottleneck = False
-        self.f = f()
-        self.instnorm = nn.InstanceNorm2d(1)
-        self.pad = nn.ReplicationPad2d(int((kernel_size-1)/2))
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=channels[-1], kernel_size=kernel_size)
-        self.bn1 = nn.BatchNorm2d(channels[-1])
-        self.resblocks = nn.Sequential(*[ResBlock(kernel_size, channels, layers_per_block) for _ in range(blocks)])
-        self.conv_out = nn.Conv2d(in_channels=channels[-1], out_channels=1, kernel_size=1)
-        
-    def forward(self, q, apply_f=True, return_phi=True):
-        q = self.bn1(self.act(self.pad(self.conv1(self.instnorm(q)))))
-        phi = self.conv_out(self.resblocks(q))
-        #phi = self.resblocks(q)
-        if apply_f and return_phi:
-            return phi, self.f(phi)
-        if apply_f:
-            return self.f(phi)
-        return phi
-    
-    
-class FTR_Enc_small(BaseModule):
-    def __init__(self, n_alphas=10, output_activation=False):
-        super(self.__class__, self).__init__()
-        self.instnorm = nn.InstanceNorm2d(1)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=5)
-        self.bn1 = nn.BatchNorm2d(8)
-        
-        self.out_fc1 = nn.Linear(2704, n_alphas)
-
-
 class FTR_Enc(BaseModule):
-    def __init__(self, n_alphas=10, output_activation=False, spatial_shape=[128, 128], input_norm=nn.InstanceNorm2d):
+    def __init__(self, n_alphas=10, spatial_shape=[128, 128], input_norm=nn.InstanceNorm2d, dtype=torch.float32):
         super(self.__class__, self).__init__()
         k = 5
         s = 2
         self.spatial_shape = spatial_shape
         self.input_norm = input_norm(1)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=k)
-        self.bn1 = nn.BatchNorm2d(8)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=k, dtype=dtype)
+        self.bn1 = nn.BatchNorm2d(8, dtype=dtype)
         self.spatial_shape = [int((n - k)) + 1 for n in self.spatial_shape]
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=k, stride=s)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=k, stride=s, dtype=dtype)
+        self.bn2 = nn.BatchNorm2d(16, dtype=dtype)
         self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=k, stride=s)
-        self.bn3 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=k, stride=s, dtype=dtype)
+        self.bn3 = nn.BatchNorm2d(32, dtype=dtype)
         self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
-        self.conv4 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=k, stride=s)
-        self.bn4 = nn.BatchNorm2d(16)
+        self.conv4 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=k, stride=s, dtype=dtype)
+        self.bn4 = nn.BatchNorm2d(16, dtype=dtype)
         self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
         
-        self.out_fc1 = nn.Linear(16 * self.spatial_shape[0] * self.spatial_shape[1], 512)
-        self.bn_out_fc1 = nn.BatchNorm1d(512)
-        self.out_fc2 = nn.Linear(512, n_alphas)
+        self.out_fc1 = nn.Linear(16 * self.spatial_shape[0] * self.spatial_shape[1], 512, dtype=dtype)
+        self.bn_out_fc1 = nn.BatchNorm1d(512, dtype=dtype)
+        self.out_fc2 = nn.Linear(512, n_alphas, dtype=dtype)
 
-        # self.out_act = nn.ReLU()
-        if output_activation:
-            self.out_act = self.act
-        else:
-            self.out_act = None
     
     def forward(self, q):
         q = self.input_norm(q)
@@ -173,66 +56,14 @@ class FTR_Enc(BaseModule):
         out = q.flatten(start_dim=1)
         out = self.bn_out_fc1(self.act(self.out_fc1(out)))
         out = self.out_fc2(out)
-        out = self.out_act(out) + isinstance(self.act, nn.ELU) if self.out_act is not None else out
-        return out
-
-
-class FTR_Enc2(BaseModule):
-    def __init__(self, n_alphas=10, output_activation=False, spatial_shape=[128, 128], input_norm=nn.BatchNorm2d):
-        super(self.__class__, self).__init__()
-        k = 5
-        s = 2
-        self.spatial_shape = spatial_shape
-        self.input_norm = input_norm(1)
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=k)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.res_block1 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.spatial_shape = [int((n - k)) + 1 for n in self.spatial_shape]
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
-        self.res_block2 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.conv3 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.bn3 = nn.BatchNorm2d(16)
-        self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
-        self.res_block3 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.bn4 = nn.BatchNorm2d(16)
-        self.spatial_shape = [int((n - k) / s) + 1 for n in self.spatial_shape]
-        self.res_block4 = ResBlock(kernel_size=5, channels=16, layers=2)
-
-        self.out_fc0 = nn.Linear(16 * self.spatial_shape[0] * self.spatial_shape[1], 2048)
-        self.bn_out_fc0 = nn.BatchNorm1d(2048)
-        self.out_fc1 = nn.Linear(2048, 512)
-        self.bn_out_fc1 = nn.BatchNorm1d(512)
-        self.out_fc2 = nn.Linear(512, n_alphas)
-        
-        # self.out_act = nn.ReLU()
-        if output_activation:
-            self.out_act = self.act
-        else:
-            self.out_act = None
-    
-    def forward(self, q):
-        q = self.input_norm(q)
-        
-        # feature generation:
-        q = self.res_block1(self.bn1(self.act(self.conv1(q))))
-        q = self.res_block2(self.bn2(self.act(self.conv2(q))))
-        q = self.res_block3(self.bn3(self.act(self.conv3(q))))
-        q = self.res_block4(self.bn4(self.act(self.conv4(q))))
-        
-        out = q.flatten(start_dim=1)
-        out = self.bn_out_fc0(self.act(self.out_fc0(out)))
-        out = self.bn_out_fc1(self.act(self.out_fc1(out)))
-        out = self.out_fc2(out)
-        out = self.out_act(out) + isinstance(self.act, nn.ELU) if self.out_act is not None else out
         return out
     
     
 class FTR_Dec(BaseModule):
-    def __init__(self, n_alpha=10, f=nn.Sigmoid, learn_frontwidth=False, spatial_shape=[128, 128]):
+    def __init__(self, n_alpha=10, f=nn.Sigmoid, learn_frontwidth=False, spatial_shape=[128, 128], lam=1, dtype=torch.float32):
         super(self.__class__, self).__init__()
+
+        self.lam = lam
         
         k = 5
         s = 2
@@ -259,7 +90,6 @@ class FTR_Dec(BaseModule):
         self.tconv3 = nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=k, stride=s)
         self.bn4 = nn.BatchNorm2d(8)
         self.tconv4 = nn.ConvTranspose2d(in_channels=8, out_channels=1, kernel_size=k)
-        
         self.f = f()
     
     def forward(self, alpha, apply_f=True, return_phi=False):
@@ -288,96 +118,24 @@ class FTR_Dec(BaseModule):
             return phi
 
 
-class FTR_Dec2(BaseModule):
-    def __init__(self, n_alpha=10, f=nn.Sigmoid, learn_frontwidth=False, spatial_shape=[128, 128]):
-        super(self.__class__, self).__init__()
-        
-        k = 5
-        s = 2
-        shape0 = [spatial_shape]
-        for lay in range(4):
-            shape0.append([int((n - k) / (s if lay > 0 else 1)) + 1 for n in shape0[-1]])
-        self.spatial_shape0 = shape0[::-1]
-        self.unpad = []
-        for n in range(len(self.spatial_shape0) - 1):
-            s0 = (1 if n == (len(self.spatial_shape0) - 2) else s)
-            self.unpad.append([m - ((l - 1) * s0 + k) for l, m in zip(self.spatial_shape0[n], self.spatial_shape0[n + 1])])
-        
-        self.learn_frontwidth = learn_frontwidth
-        self.pad = nn.ReplicationPad2d(2)
-        
-        self.in_fc0 = nn.Linear(n_alpha - learn_frontwidth, 512)
-        self.bn_in_fc0 = nn.BatchNorm1d(512)
-        self.in_fc1 = nn.Linear(512, 2048)
-        self.bn_in_fc1 = nn.BatchNorm1d(2048)
-        self.in_fc2 = nn.Linear(2048, 16 * self.spatial_shape0[0][0] * self.spatial_shape0[0][1])
-        
-        
-        self.bn1 = nn.BatchNorm2d(16)
-        self.res_block1 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.tconv1 = nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.res_block2 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.tconv2 = nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.bn3 = nn.BatchNorm2d(16)
-        self.res_block3 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.tconv3 = nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=k, stride=s)
-        self.res_block4 = ResBlock(kernel_size=5, channels=16, layers=2)
-        self.bn4 = nn.BatchNorm2d(16)
-        self.tconv4 = nn.ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=k)
-        self.res_block5 = ResBlock(kernel_size=5, channels=1, layers=2, act_output=False)
-        
-        self.f = f()
-    
-    def forward(self, alpha, apply_f=True, return_phi=False):
-        lam = 1
-        if self.learn_frontwidth:
-            lam = alpha[:, [0]][..., None, None].abs()  # shape: batch, x, y
-            alpha = alpha[:, 1:]
-        
-        alpha = self.bn_in_fc0(self.act(self.in_fc0(alpha)))
-        alpha = self.bn_in_fc1(self.act(self.in_fc1(alpha)))
-        alpha = self.act(self.in_fc2(alpha))
-        
-        phi = self.bn1(alpha.reshape(-1, 16, *self.spatial_shape0[0]))
-        
-        # feature generation:
-        phi = self.bn2(self.act(self.tconv1(self.pad(self.res_block1(phi)))))[..., 4:-(4 - self.unpad[0][0]), 4:-(4 - self.unpad[0][1])]
-        phi = self.bn3(self.act(self.tconv2(self.pad(self.res_block2(phi)))))[..., 4:-(4 - self.unpad[1][0]), 4:-(4 - self.unpad[1][1])]
-        phi = self.bn4(self.act(self.tconv3(self.pad(self.res_block3(phi)))))[..., 4:-(4 - self.unpad[2][0]), 4:-(4 - self.unpad[2][1])]
-        phi = self.res_block5(self.tconv4(self.res_block4(phi)))
-        
-        if apply_f:
-            if return_phi:
-                return phi, self.f(phi * lam)
-            else:
-                return self.f(phi * lam)
-        else:
-            return phi
-
-
 class FTR_Dec_1Lay(BaseModule):
-    def __init__(self, n_alpha=10, f=nn.Sigmoid, learn_frontwidth=False, spatial_shape=[128, 128]):
+    def __init__(self, n_alpha=10, f=nn.Sigmoid, spatial_shape=[128, 128], lam=1, dtype=torch.float32):
         super(self.__class__, self).__init__()
         self.spatial_shape = spatial_shape
-        self.in_fc1 = nn.Linear(n_alpha - learn_frontwidth, self.spatial_shape[0] * self.spatial_shape[1], bias=False)
+        self.lam = lam
+        self.in_fc1 = nn.Linear(n_alpha, self.spatial_shape[0] * self.spatial_shape[1], bias=False, dtype=dtype)
+        # nn.init.constant_(self.in_fc1.weight, 0)
         self.f = f()
-        self.learn_frontwidth = learn_frontwidth
     
     def forward(self, alpha, apply_f=True, return_phi=False):
         
-        if self.learn_frontwidth:
-            lam = alpha[:, [0]][..., None, None].abs()  # shape: batch, x, y
-            phi = self.in_fc1(alpha[:, 1:]).reshape(alpha.shape[0], 1, self.spatial_shape[0], self.spatial_shape[1])
-        else:
-            lam = 1
-            phi = self.in_fc1(alpha).reshape(alpha.shape[0], 1, self.spatial_shape[0], self.spatial_shape[1])
+        phi = self.in_fc1(alpha).reshape(alpha.shape[0], 1, self.spatial_shape[0], self.spatial_shape[1])
         
         if apply_f:
             if return_phi:
-                return phi, self.f(phi * lam)
+                return phi, self.f(phi/self.lam)
             else:
-                return self.f(phi * lam)
+                return self.f(phi/self.lam)
         else:
             return phi
     
@@ -389,13 +147,11 @@ class FTR_Dec_1Lay(BaseModule):
 
 
 class FTR_AE(BaseModule):
-    def __init__(self, encoder=None, decoder=None, n_alphas=10, f=nn.Sigmoid, learn_periodic_alphas=True, alpha_act=True, spatial_shape=[128, 128]):
+    def __init__(self, encoder=None, decoder=None, n_alphas=10, f=nn.Sigmoid, spatial_shape=[128, 128], dtype=torch.float32):
         super(self.__class__, self).__init__()
         self.has_bottleneck = True
-        self.encoder = FTR_Enc(n_alphas, alpha_act, spatial_shape=spatial_shape) if encoder is None else encoder
-        self.decoder = FTR_Dec(n_alphas, f) if decoder is None else decoder
-
-        self.periodic_net = PeriodicFunction(n_alpha=n_alphas, n_freqs=n_alphas) if learn_periodic_alphas else None
+        self.encoder = FTR_Enc(n_alphas, spatial_shape=spatial_shape, dtype=dtype) if encoder is None else encoder
+        self.decoder = FTR_Dec(n_alphas, f, dtype=dtype) if decoder is None else decoder
 
     def forward(self, q, return_code=False, apply_f=True, return_phi=False):
         code = self.encoder(q)
@@ -408,14 +164,3 @@ class FTR_AE(BaseModule):
             returns += [self.decoder(code, apply_f)]
             
         return returns
-
-
-class PeriodicFunction(BaseModule):
-    def __init__(self, n_alpha=10, n_freqs=10):
-        super(self.__class__, self).__init__()
-        self.omega = nn.Linear(1, n_freqs, bias=False)
-        self.alpha = nn.Linear(2 * n_freqs, n_alpha)
-    
-    def forward(self, t):
-        om_t = self.omega(t)
-        return self.alpha(torch.cat([torch.cos(om_t), torch.sin(om_t)], -1))
--- lib/NeuralFTR/trainer.py
+++ lib/NeuralFTR/trainer.py
@@ -3,8 +3,7 @@ import torch.nn.functional as F
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-#from moving_disk_data import batch_generator
-from NeuralFTR.utils import save_codeBase, to_torch, to_numpy
+from NeuralFTR.utils import save_codeBase, to_torch
 import os
 
 try:
@@ -32,10 +31,9 @@ class Trainer(object):
                  lr=0.001,
                  lr_min=0.0001,
                  smooth_phi=0.0005,
-                 sparse_reg=1,
-                 sparsity_measure=1,
                  log_folder='./train_results_local/',
                  device=DEVICE,
+                 dtype=torch.float32
                  ):
         
         self.net = net.to(device)
@@ -45,33 +43,22 @@ class Trainer(object):
         self.train_set_params = train_set_params
         self.train_set = train_set
         self.test_set = test_set
-        #self.reconstruction_loss = torch.nn.MSELoss()
         self.optim = torch.optim.Adam(params=self.net.parameters(), lr=lr)
         self.lr_min = lr_min
         self.smooth_phi = smooth_phi
-        self.sparse_reg = sparse_reg
-        self.sparsity_measure = sparsity_measure
         self.logs_folder = log_folder
-        # self.optim = torch.optim.SGD(params=self.net.parameters(), lr=lr, momentum=0.5)
         self.device = device
+        self.dtype = dtype
         
-        # Set up a kernel to compute the 2D 2nd order central finite difference in along both axes via 2 conv2d channels
-        # self.dx = self.X[1, 0] - self.X[0, 0]
-        # self.dy = self.Y[0, 1] - self.Y[0, 0]
+        # Set up a kernel to compute the 2D 2nd order central finite difference along both axes via 2 conv2d channels
         self.dx = self.X[0, 1] - self.X[0, 0] if (self.X[0, 1] - self.X[0, 0]) != 0 else self.X[1, 0] - self.X[0, 0]
         self.dy = self.Y[1, 0] - self.Y[0, 0] if (self.Y[1, 0] - self.Y[0, 0]) != 0 else self.Y[0, 1] - self.Y[0, 0]
-        self.D_kernel = torch.zeros([2, 3, 3], device=self.device, dtype=torch.float32)
-        self.Dx_kernel = torch.ones([1, 1, 2, 1], device=self.device, dtype=torch.float32) / self.dx
-        self.Dy_kernel = torch.ones([1, 1, 1, 2], device=self.device, dtype=torch.float32) / self.dy
+        self.Dx_kernel = torch.ones([1, 1, 2, 1], device=self.device, dtype=dtype) / self.dx
+        self.Dy_kernel = torch.ones([1, 1, 1, 2], device=self.device, dtype=dtype) / self.dy
         self.pady = torch.nn.ReplicationPad2d(padding=[1, 0, 0, 0])
         self.padx = torch.nn.ReplicationPad2d(padding=[0, 0, 1, 0])
         self.Dx_kernel[0, 0, 0, 0] *= -1
         self.Dy_kernel[0, 0, 0, 0] *= -1
-        self.D_kernel[[0, 1], [0, 1], [1, 0]] = -1
-        self.D_kernel[[0, 1], [-1, 1], [1, -1]] = 1
-        self.D_kernel[0, ...] /= 2 * self.dx
-        self.D_kernel[1, ...] /= 2 * self.dy
-        self.D_kernel = self.D_kernel[:, None, ...]
         
     def test(self, data, batch_size=None):
         data_len = data.shape[0]
@@ -91,25 +78,27 @@ class Trainer(object):
                 test_loss = self.get_loss(data, grad=False)
         self.net.train() # set net back to training mode
         return test_loss
+
+    def get_reco_error(self, data):
+        self.net.eval()
+        with torch.no_grad():
+            reco = self.net(data)[0]
+        self.net.train()
+        return torch.norm(data - reco, p='fro') / torch.norm(data, p='fro')
     
     def reconstruction_loss(self, q_hat, q):
         reco_loss = (torch.norm((q_hat - q).flatten(1), p='fro') / torch.norm(q.flatten(1), p='fro')) ** 2
-        if not reco_loss.isfinite().all():
-            debug=1
         return reco_loss
         
     def smoothness_loss(self, field):
+        field_norm = torch.norm(field.flatten(1), p='fro')
+        if field_norm == 0:
+            return 0
         dfield_dx = F.conv2d(self.padx(field), self.Dx_kernel)
         dfield_dy = F.conv2d(self.pady(field), self.Dy_kernel)
         abs_grad_field = (torch.cat([dfield_dx, dfield_dy], 1)**2).sum(1).sqrt()
-        # smoothness_loss = ((torch.norm(abs_grad_field, p='fro', dim=(-2, -1)) / torch.norm(field, p='fro'))**2).mean()
-        smoothness_loss = (torch.norm(abs_grad_field.flatten(1), p='fro') / torch.norm(field.flatten(1), p='fro')) ** 2
-        if not smoothness_loss.isfinite().all():
-            debug=1
+        smoothness_loss = (torch.norm(abs_grad_field.flatten(1), p='fro') / field_norm) ** 2
         return smoothness_loss
-    
-    def sparsity_loss(self, x):
-        return (torch.norm(x.flatten(1), p=self.sparsity_measure) / torch.norm(x.flatten(1), p='fro')) ** 2
     
         
     def get_loss(self, batch, grad=True):
@@ -129,13 +118,6 @@ class Trainer(object):
                     loss += self.smoothness_loss(modes) * self.smooth_phi
                 else:
                     loss += self.smoothness_loss(phi) * self.smooth_phi
-
-            if self.sparse_reg:
-                if self.sparsity_measure == 'nuc' or not self.net.has_bottleneck:
-                    loss += self.sparsity_loss(phi) * self.sparse_reg
-                else:
-                    loss += self.sparsity_loss(code) * self.sparse_reg
-                    loss += self.declining_code_loss(code) * self.sparse_reg
             return loss
         
         if grad:
@@ -145,48 +127,36 @@ class Trainer(object):
             with torch.no_grad():
                 loss = loss_fn(batch)
                 return loss
-    
-    def resume_training(self, log_base_name):
-        same_name = [dir_name for dir_name in os.listdir(self.logs_folder) if log_base_name in dir_name]#[-1]
-        # TBD
         
     def get_batch(self, step, batch_size):
         idx = step % self.train_set.shape[0]
         batch_data = self.train_set.roll(-idx, 0)[:batch_size, ...]
         return batch_data
     
-    def training(self, batch_size, trainsteps=1e5, test_every=1e3, save_every=5e3, test_batch_size=None, log_base_name='', resume=False):
+    def training(self, batch_size, trainsteps=1e5, test_every=1e3, save_every=5e3, test_batch_size=None, log_base_name=''):
         log_folder = self.logs_folder + log_base_name + time.strftime("%Y_%m_%d__%H-%M", time.localtime()) + '/'
         if not os.path.isdir(log_folder):
             os.makedirs(log_folder)
-        if resume:
-            self.resume_training(log_base_name)
+
         self.tensorboard = SummaryWriter(log_dir=log_folder) if TB_MODE else None
         save_codeBase(os.getcwd(), log_folder)
 
         #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optim, trainsteps, self.lr_min)
-        lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optim, factor=0.5, patience=500, cooldown=1000, verbose=True)
+        #lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optim, factor=0.5, patience=500, cooldown=1000, verbose=True)
         
         test_loss = torch.zeros(1)
         train_loss_log = []
         test_loss_log = []
         
-        # if self.train_set_params is not None:
-        #     batch_gen = batch_generator(self.train_set_params, self.X, self.Y, batch_size, device=DEVICE)
-        
-        
         #train loop
         best_so_far = 1e12
         for step in range(int(trainsteps)):
             
-            #learn:
-            # if self.train_set_params is not None:
-            #     batch = next(batch_gen)
-            # else:
             batch = self.get_batch(step, batch_size)
             loss = self.get_loss(batch)
             self.optim.zero_grad()
             loss.backward()
+            
             self.optim.step()
             
             train_loss_log.append([loss.item(), step])
@@ -219,17 +189,10 @@ class Trainer(object):
                 self.tensorboard.add_scalar('train_loss', loss, step+1)
                 self.tensorboard.flush()
                 
-            lr_scheduler.step(loss)
+            #lr_scheduler.step(loss)
             if step % 100 == 0:
                 print(f"{step}: loss={loss.item():1.5}; test_loss={test_loss.item():1.5}", end="\r")
         return np.array(train_loss_log), np.array(test_loss_log)
-    
-    def get_reco_error(self, data):
-        self.net.eval()
-        with torch.no_grad():
-            reco = self.net(data)[0]
-        self.net.train()
-        return torch.norm(data-reco, p='fro')/torch.norm(data, p='fro')
     
     def plot_test_idx_reco(self, plot_idx=None):
         plot_idx = np.random.randint(self.test_set.shape[0]) if plot_idx is None else plot_idx
@@ -254,7 +217,7 @@ class Trainer(object):
 
 def plot_reconstrcution(truth, reco, phi=None):
     p1 = (phi is not None) * 1
-    fig, axes = plt.subplots(1, 3 + p1, num=0, figsize=[19.2, 10.8])
+    fig, axes = plt.subplots(1, 3 + p1, num=0, figsize=[19.2, 3.6])
     dims_remove = [0] * (truth.ndim - 2) + [...]
     if p1:
         pc = axes[0].imshow(phi[dims_remove].to('cpu').T, origin='lower')
